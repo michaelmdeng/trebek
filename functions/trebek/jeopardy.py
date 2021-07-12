a92112e52f580edad60b4ab9a4bdc505d8cb1c3b
Michael Deng <33045922+michaelmdeng@users.noreply.github.com>
2021-07-11 21:54:20 -0700

    Add notification that chief may be on shift (#13)
    * Add notification that chief may be on shift
    
    * black fixes

--- functions/trebek/jeopardy.py
+++ functions/trebek/jeopardy.py
@@ -5,3 +5,7 @@ Helpers for working with Jeopardy shifts on a calendar.
 
 def is_jeop_chief_shift(event):
     return event["summary"] == "Jeop Chief"
+
+
+def is_em_shift(event):
+    return event["summary"] != "Jeop Chief"
