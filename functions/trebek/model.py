540ff978b0ec7bd3e12c9582c79b21eedd7bc2c9
Michael Deng <mdeng@squareup.com>
2021-07-21 20:01:24 -0700

    Migrate namedtuple to dataclass and type-class everything

--- functions/trebek/model.py
+++ functions/trebek/model.py
@@ -1,4 +1,4 @@
-from collections import namedtuple
+from dataclasses import asdict, dataclass, replace
 from datetime import datetime
 import logging
 
@@ -6,68 +6,76 @@ from google.cloud import datastore
 
 DATASTORE_CLIENT = None
 
-ForwardingInfo = namedtuple(
-    "ForwardingInfo",
-    [
-        "name",  # name of the person to forward to
-        "forwardTo",  # phone number of the person to forward to
-        "onShift",  # whether the person forwarding to is on shift
-    ],
-)
 
-
-class ForwardingStatusContext:
-    def __init__(self, data):
-        self._id = None
-        self._status = ForwardingStatus.from_data(data)
-
-    def __enter__(self):
-        self._status = self._status._replace(
-            id=self._id,
-            status=ForwardingStatus.START_STATUS,
-            startTime=datetime.utcnow(),
-        )
-        self._id = self._status.save().id
-
-    def __exit__(self, type, exc, traceback):
-        if type is None:
-            self._status = self._status._replace(
-                id=self._id,
-                status=ForwardingStatus.SUCCESS_STATUS,
-                endTime=datetime.utcnow(),
-            )
-            self._id = self._status.save().id
-            return True
-        else:
-            self._status = self._status._replace(
-                id=self._id,
-                status=ForwardingStatus.ERROR_STATUS,
-                endTime=datetime.utcnow(),
-                error=exc.__str__(),
-            )
-            self._id = self._status.save().id
-            return False
-
-
-class ForwardingStatus(
-    namedtuple(
-        "ForwardingStatus",
-        ["id", "requestData", "startTime", "endTime", "status", "error"],
-    )
-):
+@dataclass
+class ForwardingRequest:
     """
-    Describes status of a call-forwarding operation. Persisted to Google Cloud
+    Request to forward a call to the current Jeopardy chief.
+    """
+
+    numberFrom: str
+    numberTo: str
+    requestTime: datetime = datetime.utcnow()
+
+
+@dataclass
+class ScheduleConfig:
+    """
+    Scheduling config for a given person.
+    """
+
+    name: str
+    phoneNumber: str
+    calendarId: str
+
+
+@dataclass
+class JeopardyScheduleConfig:
+    """
+    Scheduling config for a Jeopardy Chief.
+    """
+
+    name: str
+    phoneNumber: str
+    calendarId: str
+    onShift: bool
+
+
+@dataclass
+class ForwardingResponse:
+    """
+    Response that indicates the current Jeopardy chief to forward the call to.
+    """
+
+    name: str
+    forwardTo: str
+    onShift: bool
+
+
+@dataclass
+class ForwardingStatus:
+    """
+    Status of a call-forwarding operation. Persisted to Google Cloud
     Datastore using save(). Implements context manager to enable saving and
     updating the operation status on start, completion, or error.
     """
+
+    id: str
+    requestData: str
+    startTime: datetime
+    endTime: datetime
+    status: str
+    error: str = ""
 
     START_STATUS = "START"
     SUCCESS_STATUS = "SUCCESS"
     ERROR_STATUS = "ERROR"
 
     @staticmethod
-    def from_data(form_data):
-        return ForwardingStatus(None, form_data, None, None, None, None)
+    def from_request(request):
+        return ForwardingStatus(
+            id=None, requestData=request, startTime=None, endTime=None, status=None
+        )
 
     def save(self):
         global DATASTORE_CLIENT
@@ -81,8 +89,44 @@ class ForwardingStatus(
             key = client.key("ForwardingStatus")
 
         entity = datastore.Entity(key)
-        entity.update(self._asdict())
+        entity.update(asdict(self))
 
         logging.debug("Saving ForwardingStatus to Datastore: %s", entity)
         client.put(entity)
         return entity
+
+
+class ForwardingStatusContext:
+    def __init__(self, request):
+        self._id = None
+        self._status = ForwardingStatus.from_request(request)
+
+    def __enter__(self):
+        self._status = replace(
+            self._status,
+            id=self._id,
+            status=ForwardingStatus.START_STATUS,
+            startTime=datetime.utcnow(),
+        )
+        self._id = self._status.save().id
+
+    def __exit__(self, type, exc, traceback):
+        if type is None:
+            self._status = replace(
+                self._status,
+                id=self._id,
+                status=ForwardingStatus.SUCCESS_STATUS,
+                endTime=datetime.utcnow(),
+            )
+            self._id = self._status.save().id
+            return True
+        else:
+            self._status = replace(
+                self._status,
+                id=self._id,
+                status=ForwardingStatus.ERROR_STATUS,
+                endTime=datetime.utcnow(),
+                error=exc.__str__(),
+            )
+            self._id = self._status.save().id
+            return False
