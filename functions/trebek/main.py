c31f6ee7a56a4657f15977a5bb676e5e42f65f39
Michael Deng <33045922+michaelmdeng@users.noreply.github.com>
2021-07-21 17:39:50 -0700

    Write forwarding status to datastore (#24)

--- functions/trebek/main.py
+++ functions/trebek/main.py
@@ -7,7 +7,7 @@ import werkzeug.exceptions
 import gcal
 import config
 import jeopardy
-from model import ForwardingInfo
+from model import ForwardingInfo, ForwardingStatusContext
 
 
 logging.basicConfig(level=logging.INFO)
@@ -18,19 +18,19 @@ def trebek(request):
     logging.info("Received request with data: %s", form_data)
 
     try:
-        call_from = form_data["callFrom"]
-        call_to = form_data["callTo"]
-        call_time = datetime.utcnow()
-    except werkzeug.exceptions.BadRequestKeyError as e:
-        return flask.make_response(flask.jsonify({"error": e.__str__()}), 400)
+        with ForwardingStatusContext(form_data):
+            call_from = form_data["callFrom"]
+            call_to = form_data["callTo"]
+            call_time = datetime.utcnow()
 
-    try:
-        forward_info = get_forward_info(call_from, call_to, call_time)
+            forward_info = get_forward_info(call_from, call_to, call_time)
+            return flask.make_response(flask.jsonify(forward_info._asdict()), 200)
+    except werkzeug.exceptions.BadRequestKeyError as e:
+        logging.exception(e)
+        return flask.make_response(flask.jsonify({"error": e.__str__()}), 400)
     except Exception as e:
         logging.exception(e)
         return flask.make_response(flask.jsonify({"error": e.__str__()}), 500)
-
-    return flask.make_response(flask.jsonify(forward_info._asdict()), 200)
 
 
 def get_forward_info(call_from, call_to, call_time):
--- functions/trebek/model.py
+++ functions/trebek/model.py
@@ -1,4 +1,10 @@
 from collections import namedtuple
+from datetime import datetime
+import logging
+
+from google.cloud import datastore
+
+DATASTORE_CLIENT = None
 
 ForwardingInfo = namedtuple(
     "ForwardingInfo",
@@ -8,3 +14,75 @@ ForwardingInfo = namedtuple(
         "onShift",  # whether the person forwarding to is on shift
     ],
 )
+
+
+class ForwardingStatusContext:
+    def __init__(self, data):
+        self._id = None
+        self._status = ForwardingStatus.from_data(data)
+
+    def __enter__(self):
+        self._status = self._status._replace(
+            id=self._id,
+            status=ForwardingStatus.START_STATUS,
+            startTime=datetime.utcnow(),
+        )
+        self._id = self._status.save().id
+
+    def __exit__(self, type, exc, traceback):
+        if type is None:
+            self._status = self._status._replace(
+                id=self._id,
+                status=ForwardingStatus.SUCCESS_STATUS,
+                endTime=datetime.utcnow(),
+            )
+            self._id = self._status.save().id
+            return True
+        else:
+            self._status = self._status._replace(
+                id=self._id,
+                status=ForwardingStatus.ERROR_STATUS,
+                endTime=datetime.utcnow(),
+                error=exc.__str__(),
+            )
+            self._id = self._status.save().id
+            return False
+
+
+class ForwardingStatus(
+    namedtuple(
+        "ForwardingStatus",
+        ["id", "requestData", "startTime", "endTime", "status", "error"],
+    )
+):
+    """
+    Describes status of a call-forwarding operation. Persisted to Google Cloud
+    Datastore using save(). Implements context manager to enable saving and
+    updating the operation status on start, completion, or error.
+    """
+
+    START_STATUS = "START"
+    SUCCESS_STATUS = "SUCCESS"
+    ERROR_STATUS = "ERROR"
+
+    @staticmethod
+    def from_data(form_data):
+        return ForwardingStatus(None, form_data, None, None, None, None)
+
+    def save(self):
+        global DATASTORE_CLIENT
+        if DATASTORE_CLIENT is None:
+            DATASTORE_CLIENT = datastore.Client()
+        client = DATASTORE_CLIENT
+
+        if self.id is not None:
+            key = client.key("ForwardingStatus", self.id)
+        else:
+            key = client.key("ForwardingStatus")
+
+        entity = datastore.Entity(key)
+        entity.update(self._asdict())
+
+        logging.debug("Saving ForwardingStatus to Datastore: %s", entity)
+        client.put(entity)
+        return entity
