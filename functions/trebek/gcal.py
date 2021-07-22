84d746710e0a03d05147b09a8623f454861a5684
Michael Deng <33045922+michaelmdeng@users.noreply.github.com>
2021-07-21 18:18:24 -0700

    Make requests to calendar API async (#26)

--- functions/trebek/gcal.py
+++ functions/trebek/gcal.py
@@ -1,20 +1,40 @@
 from datetime import datetime, timedelta
+import json
+import logging
 
-from google.oauth2.service_account import Credentials
-from googleapiclient.discovery import build
+from aiogoogle import Aiogoogle
+from aiogoogle.auth.creds import ServiceAccountCreds
 
 
 class GCalClient:
     def __init__(self):
-        creds = None
-        creds = Credentials.from_service_account_file("credentials.json")
+        self._creds_file = None
+        self._google = None
 
-        self._service = build("calendar", "v3", credentials=creds)
+    async def __aenter__(self):
+        self._creds_file = open("credentials.json")
+        service_account_key = json.load(self._creds_file)
+        creds = ServiceAccountCreds(
+            scopes=["https://www.googleapis.com/auth/calendar.events.readonly"],
+            **service_account_key
+        )
 
-    def get_events(self, cal_id="primary", event_time=datetime.utcnow()):
-        events_result = (
-            self._service.events()
-            .list(
+        self._google = Aiogoogle(service_account_creds=creds)
+        await self._google.__aenter__()
+
+        return self
+
+    async def __aexit__(self, exc_type, exc, backtrace):
+        if self._creds_file:
+            self._creds_file.close()
+        if self._google:
+            await self._google.__aexit__(exc_type, exc, backtrace)
+
+    async def get_events_async(self, cal_id="primary", event_time=datetime.utcnow()):
+        cal = await self._google.discover("calendar", "v3")
+        logging.debug("Checking calendar events for id: %s at: %s", cal_id, event_time)
+        events_result = await self._google.as_service_account(
+            cal.events.list(
                 calendarId=cal_id,
                 timeMin=(event_time - timedelta(minutes=1)).isoformat() + "Z",
                 timeMax=(event_time + timedelta(minutes=1)).isoformat() + "Z",
@@ -22,7 +42,6 @@ class GCalClient:
                 singleEvents=True,
                 orderBy="startTime",
             )
-            .execute()
         )
 
         return events_result.get("items", [])
